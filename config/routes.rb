fba96f1d2053ea46a4ad31277ae512da8cb56671
Dernise <dernise@live.fr>
2014-09-24 17:22:27 +0200

    Added bootstrap + working register page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get 'pages/contact'
   get 'pages/tos'
 
-  resources :user
+  #Sign up
+  get '/register' => 'users#new', as: :register
+
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
