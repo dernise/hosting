3d8d44d0f5c721aa846cf5e0d724f285d9248dd2
Maxence Henneron <dernise@live.fr>
2014-09-24 18:22:45 +0200

    Fix indent problems

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,7 @@
 (function($) {
     $(document).ready(function() {
-
+        //Remove white space in register page if screen is too big
+        //$('#register-content').css('margin-bottom', ());
     });
 })(jQuery);
 
