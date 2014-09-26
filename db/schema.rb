ae672698b0af4bd3f047650415ddd09885c685e9
Braems Romain <rb62680@gmail.com>
2014-09-26 10:54:54 +0200

    Add Tokens collumn to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923081529) do
+ActiveRecord::Schema.define(version: 20140926084914) do
 
   create_table "users", force: true do |t|
     t.string   "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140923081529) do
     t.string   "address"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "tokens"
   end
 
 end
