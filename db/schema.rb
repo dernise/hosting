114494b6135a7b44c6da7d11f9dc8ddc31e8340b
Dernise <dernise@live.fr>
2014-10-02 08:41:10 +0200

    Header now redirect to root_path
    * Removed cart schema
    * Added link to root_path

--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20141001083240) do
 
-  create_table "carts", force: true do |t|
-    t.datetime "purchased_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "messages", force: true do |t|
     t.string   "content"
     t.integer  "ticket_id"
